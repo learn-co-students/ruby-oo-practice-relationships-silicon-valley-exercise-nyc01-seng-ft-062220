f34c11be36b8faf854b2283af24bde06a31988df
Jeff-Adler <Jeffm.adler@gmail.com>
2020-06-26 13:58:42 -0400

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,85 @@
-class Startup
+require 'pry'
 
+require '../models/funding_round.rb'
+require '../models/venture_capitalist.rb'
+
+
+class Startup
+  attr_reader :founder, :domain
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+    @@all << self
+  end
+
+  def pivot(domain, name)
+    @domain = domain
+    @name = name
+  end
+
+  def self.find_by_founder(name)
+    @@all.find do |element|
+      element.name == name
+    end
+  end
+
+  def self.domains
+    @@all.map do |element|
+      element.domain
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+  def sign_contract(venture_capitalist, type, investment)
+    FundingRound.new(venture_capitalist, self, type, investment)
+  end
+
+  def num_funding_rounds
+    array = FundingRound.all.select do |element|
+      element.startup == self
+    end
+    array.length
+  end
+
+  def total_funds
+    array = FundingRound.all.select do |element|
+      element.startup == self
+    end
+    total = 0
+    array.each do |element|
+      total += element.investment
+    end
+    total
+  end
+
+  #Returns a unique array of all the venture capitalists that have invested in this company
+  def investors
+    array = FundingRound.all.select do |element|
+      element.startup == self
+    end
+    array2 = array.map do |element|
+      element.venture_capitalist
+    end
+    array2.uniq
+  end
+
+  def big_investors
+    array1 = investors
+    array2 = VentureCapitalist.tres_commas_club
+    array1.select do |element|
+      array2.include?(element)
+    end
+  end
 end
+
+
+
+
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,74 @@
+require 'pry'
+
+require_relative '../models/funding_round.rb'
+require_relative '../models/startup.rb'
+
+
 class VentureCapitalist
 
+  @@all = []
+
+  def initialize(name, total_worth)
+    @name = name
+    @total_worth = total_worth
+    @@all << self
+  end
+
+  def name
+    @name
+  end
+
+  def total_worth
+    @total_worth
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.tres_commas_club
+    @@all.select do |element|
+      element.total_worth > 1000000000
+    end
+  end
+
+  def offer_contract(startup, type, amount)
+    FundingRound.new(self, startup, type, amount)
+  end
+
+  def funding_rounds
+    FundingRound.all.select do |element|
+      element.venture_capitalist == self
+    end
+  end
+
+  def portfolio
+    array = self.funding_rounds.map do |element|
+      element.startup
+    end
+    array.uniq
+  end
+
+  def biggest_investment
+    max_amount = 0
+    max_funding_round = self.funding_rounds[0]
+    self.funding_rounds.each do |element|
+      if element.investment > max_amount
+        max_amount = element.investment
+        max_funding_round = element
+      end
+    end
+    max_funding_round
+  end
+
+  def invested(domain)
+    amount = 0
+    self.funding_rounds.each do |element|
+      if element.startup.domain == domain
+        amount += element.investment
+      end
+    end
+    amount
+  end
 end
+
