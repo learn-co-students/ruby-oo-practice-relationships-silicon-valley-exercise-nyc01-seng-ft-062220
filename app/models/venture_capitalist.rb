6f791716ba45aa6817580923333410231f35b88a
Alex <alex.beciana@gmail.com>
2020-07-20 23:16:28 -0400

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,60 @@
+require "pry"
+
 class VentureCapitalist
+
+    attr_reader :name
+    attr_accessor :total_worth
+
+    @@all = []
+
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def funding_rounds
+        FundingRound.all.select do |round|
+            if round.venture_capitalist == self
+                round.venture_capitalist
+            end
+        end
+    end
+
+    def self.tres_commas_club
+        self.all.collect do |vc|
+            if vc.total_worth > 1000000000
+                vc
+            end
+        end
+    end
+
+    def offer_contract(startup, type, investment)
+        FundingRound.new(startup, self, type, investment)
+    end
+
+    def portfolio
+        funding_rounds.select do |round|
+            round.startup
+        end.uniq
+    end
+
+    def biggest_investment
+        funding_rounds.max do |a, b|
+            a.investment <=> b.investment
+        end
+    end
+
+    def invested(domain)
+        funding_rounds.sum do |round|
+            if round.startup.domain == domain
+                round.investment
+            end
+        end
+    end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,3 +1,4 @@
+require "pry"
 require_relative '../config/environment.rb'
 
 def reload
@@ -6,6 +7,51 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+
+
+# (name, founder, domain)
+su1 = Startup.new("business", "Alex", "tech")
+su2 = Startup.new("apps", "Avi", "music")
+su3 = Startup.new("copycat", "N/A", "fake")
+su4 = Startup.new("stuff", "Alex", "tech")
+
+# (name, total_worth)
+vc1 = VentureCapitalist.new("Bill", 1000000001)
+vc2 = VentureCapitalist.new("Rob", 999999999)
+
+# (startup, venture_capitalist, type, investment)
+fr1 = FundingRound.new(su1, vc1, "A", 60000)
+fr2 = FundingRound.new(su2, vc2, "A", 10000)
+fr3 = FundingRound.new(su3, vc2, "Seed", -10)
+fr4 = FundingRound.new(su4, vc1, "A", 10000)
+
+Startup.find_by_founder("Alex")
+
+Startup.domains
+
+su3.pivot("news", "original")
+
+VentureCapitalist.tres_commas_club
+
+newone = su1.sign_contract(vc2, "B", 10)
+
+su1.funding_rounds
+
+su1.num_funding_rounds
+
+su1.total_funds
+
+su1.investors
+
+su1.big_investors
+
+vc2.funding_rounds
+
+vc1.portfolio
+
+vc2.biggest_investment
+
+vc1.invested("tech")
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
